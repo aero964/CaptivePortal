18d772452fc57936a6551ece62b6637b26908753
aero964 <jsp1440@outlook.com>
2024-09-14 01:20:22 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+﻿<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['agree'] == 1) {
+
+    // クライアントのIPアドレスを取得
+    $client_ip = $_SERVER['REMOTE_ADDR'];
+
+    // ARPテーブルからMACアドレスを取得
+    $arp = `/usr/sbin/arp -n $client_ip`;
+    $lines = explode("\n", $arp);
+
+    // ARPの結果からMACアドレスを抽出
+    foreach ($lines as $line) {
+        if (strpos($line, $client_ip) !== false) {
+            $cols = preg_split('/\s+/', trim($line));
+            $mac_address = $cols[2]; // MACアドレスは通常第3列
+            echo "あなたのMACアドレス: " . $mac_address;
+            break;
+        }
+    }
+
+    shell_exec("sudo /sbin/iptables -t nat -I PREROUTING 1 -i eth1 -m mac --mac-source $mac_address -j ACCEPT");
+
+
+echo <<<EOM
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="UTF-8">
+<div align=center>
+<h1>CAPTIVE PORTAL</h1>
+<hr>
+認証が完了しました
+EOM;
+
+
+    exit();
+}
+
+?>
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="UTF-8">
+<div align=center>
+<?php
+var_dump($_POST);
+?>
+        <h1>CAPTIVE PORTAL</h1>
+        <hr>
+<p>同意しちゃいますか？</p>
+<form action="" method="POST">
+        <input type="hidden" name="agree" value="1">
+        <input type="submit" value="　　　同意する　　　">
+</form>
+</div>
---
+++ readme.md
@@ -0,0 +1,30 @@
+﻿# 作業メモ
+
+1. CaptivePortal用nginx, PHPインストール
+
+2. NW
+   - eth0 WAN 10.7.0.216/24
+   - eth1 LAN 192.268.234.250/24
+       - ※eth1にはDHCPを設定した
+
+3. IPv4 Forwarding
+   - `/etc/sysctl.conf`
+       - `net.ipv4.ip_forward = 1`
+   - `sysctl -p`
+
+
+4. iptablesはサービス化した
+
+5. iptablesチェーン
+   - # 未認証のクライアントの80番、443番、その他のTCPトラフィックをキャプティブポータルへリダイレクト
+        - `iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j DNAT --to-destination 192.168.234.250:80`
+        - `iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 443 -j DNAT --to-destination 192.168.234.250:443`
+
+   - # chrome対策
+        - `iptables -t nat -A PREROUTING -i eth1 -p udp --dport 80 -j DNAT --to-destination 192.168.234.250:80`
+        - `iptables -t nat -A PREROUTING -i eth1 -p udp --dport 443 -j DNAT --to-destination 192.168.234.250:443`
+
+   - # 80/443以外の全てのTCPトラフィックをリダイレクト
+        - `iptables -t nat -A PREROUTING -i eth1 -p tcp -j DNAT --to-destination 192.168.234.250:80`
+
+###### とりあえずここまで...
